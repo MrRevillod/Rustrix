9d61840db0f778e6005e6adc4bee5de3371a9ffa
MrRevillod <lrevillod2022@alu.uct.cl>
2023-06-05 10:48:15 -0400

    utils abs

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use matrix::Matrix;
 mod menu;
 use menu::Menu;
 
+mod utils;
+
 fn main() {
     let mut menu = Menu::new(); menu.run();
 }
--- src/menu.rs
+++ src/menu.rs
@@ -1,21 +1,20 @@
+
 use dialoguer::{Input, Select};
-use std::process;
-use std::io::{self, Write};
+use colored::*;
 
 use crate::Matrix;
+use crate::utils;
 
-pub struct Menu {
-    run: bool,
-}
+pub struct Menu;
 
 impl Menu {
 
     pub fn new() -> Menu {
-        Menu { run: false }
+        Menu
     }
 
     pub fn create_menu(&self) -> usize {
-        self.clear_term();
+        utils::clear_term();
 
         let options = &[
             " [1]. Calcular Determinante           ",
@@ -35,10 +34,9 @@ impl Menu {
     }
 
     pub fn run(&mut self) {
-
         loop {
             let select = self.create_menu();
-            self.clear_term();
+            utils::clear_term();
 
             match select {
                 0 => self.get_det(),
@@ -59,11 +57,11 @@ impl Menu {
                 .interact()
                 .unwrap();
 
-            if repeat.to_ascii_lowercase() != "s" {
+            if repeat.to_lowercase() != "s" {
                 self.bye();
                 break;
             } else {
-                self.clear_term();
+                utils::clear_term();
             }
         }
     }
@@ -72,109 +70,24 @@ impl Menu {
         println!("\n [-] ¡Hasta luego!\n");
     }
 
-    pub fn clear_term(&self) {
-
-        if cfg!(target_os = "windows") {
-            let _ = process::Command::new("cmd").arg("/c").arg("cls").status();
-        } else {
-            let _ = process::Command::new("clear").status();
-        }
-    }
-
-    pub fn get_integer(&self, message: &str) -> i32 {
-
-        loop {
-            print!("{}", message);
-            io::stdout().flush().unwrap();
-            let mut input = String::new();
-            io::stdin().read_line(&mut input).unwrap();
-            match input.trim().parse::<i32>() {
-                Ok(num) => return num,
-                Err(_) => {
-                    println!("\n [-] Error: Debes ingresar un número entero.\n");
-                }
-            }
-        }
-    }
-
-    pub fn get_float(&self, message: &str) -> f64 {
-
-        loop {
-            print!("{}", message);
-            io::stdout().flush().unwrap();
-            let mut input = String::new();
-            io::stdin().read_line(&mut input).unwrap();
-            let trimmed_input = input.trim();
-
-            if let Ok(num) = trimmed_input.parse::<f64>() {
-                return num;
-            } else if let Ok(num) = trimmed_input.parse::<i32>() {
-                return num as f64;
-            } else {
-                println!("\n [-] Error: Debes ingresar un número.\n");
-            }
-        }
-    }
-
-    pub fn get_cant(&self) -> i32 {
-        let num = self.get_integer(" [-] Cant. de matrices con las que vas a operar: ");
-        println!(" ");
-
-        num
-    }
-
-    pub fn get_dimensions(&self) -> (usize, usize) {
-        let rows = self.get_integer(" [-] Cantidad de filas de tu matriz: ") as usize;
-        let cols = self.get_integer(" [-] Cantidad de columnas de tu matriz: ") as usize;
-        println!(" ");
-
-        (rows, cols)
-    }
-
-    pub fn create_matrix(&self, shape: (usize, usize)) -> Matrix {
- 
-        let mut array: Vec<Vec<f64>> = Vec::new();
-
-        for i in 0..shape.0 {
-            let mut row: Vec<f64> = Vec::new();
-
-            for j in 0..shape.1 {
-                let element = self.get_float(&format!(" [»] Ingrese el elemento ({},{}): ", i + 1, j + 1));
-                row.push(element);
-            }
-
-            array.push(row);
-        }
-
-        println!(" ");
-
-        Matrix::new(array)
-    }
-
     pub fn get_det(&self) {
-
-        let shape = self.get_dimensions();
+        let shape = utils::get_dimensions();
         if shape.0 != shape.1 {
             println!("\n [-] Para calcular determinante la matríz debe ser cuadrada. \n");
             return;
         }
 
-        let mut matrix = self.create_matrix(shape);
+        let mut matrix = utils::create_matrix(shape);
         let det = matrix.det();
 
         println!(" [-] El determinante de la matríz es: {}", det);
     }
 
     pub fn get_trans(&self) {
+        let shape = utils::get_dimensions();
+        let matrix = utils::create_matrix(shape);
 
-        let shape = self.get_dimensions();
-        let matrix = self.create_matrix(shape);
-
-        let trans = matrix.transposed();
-
-        trans.show();
+        let transposed = matrix.transposed();
+        transposed.show();
     }
 }
-
-
-
